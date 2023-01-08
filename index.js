119d3181d435d11e40f83acd8c6cb3ba6b9b207d
Alireza Balaei <alirezabalayi7@gmail.com>
2023-01-08 18:40:13 +0330

    send kline data from binance symbol & interval api using got library

--- index.js
+++ index.js
@@ -5,7 +5,29 @@ const server = app.listen(
   3000,
   log("proxy server is running on port: 3000", error)
 )
+const got = require("got")
 
-app.get("/", (req, res) => {
-  res.status(200).send("proxy server works")
+app.get("/:symbol/:interval", async (req, res) => {
+  try {
+    const { symbol, interval } = req.params
+    const resp = await got(
+      `https://api.binance.com/api/v3/klines?symbol=${symbol}&interval=${interval}`
+    )
+    // data is in format of: [[kline opentime,
+    // Open price, High price, Low price, Close price,
+    // Volume, Kline Close time, Quote asset volume,
+    // Number of trades, Taker buy base asset volume,
+    // Taker buy quote asset volume, Unused field, ignore]]
+    const data = JSON.parse(resp.body)
+    const klinedata = data.map((d) => ({
+      time: d[0] / 1000,
+      open: d[1] * 1,
+      high: d[2] * 1,
+      low: d[3] * 1,
+      close: d[4] * 1,
+    }))
+    res.status(200).json(klinedata)
+  } catch (error) {
+    res.send(error)
+  }
 })
